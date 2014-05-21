7fc1919dc78d2f14d1679b34ace735efecd3d555
Vincent Pochet <vincent.pochet@gmail.com>
2014-05-21 18:02:28 +0200

    Convert json into string

--- app/response.rb
+++ app/response.rb
@@ -5,7 +5,7 @@ class Response
     hall_url = "https://hall.com/api/1/services/generic/#{token}"
     uri = URI(hall_url)
     req = Net::HTTP::Post.new(hall_url, {'Content-Type' =>'application/json'})
-    req.body = data
+    req.body = data.to_s
     Net::HTTP.new(uri.host, uri.port, :use_ssl => true).start {|http| http.request(req) }
   end
 end
