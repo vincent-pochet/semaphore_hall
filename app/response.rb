d209c4d1ea6070716deaeeb2634fbf57bc55f80a
Vincent Pochet <vincent.pochet@gmail.com>
2014-05-21 18:04:18 +0200

    Fix start call

--- app/response.rb
+++ app/response.rb
@@ -5,8 +5,8 @@ class Response
     hall_url = "https://hall.com/api/1/services/generic/#{token}"
     uri = URI(hall_url)
     req = Net::HTTP::Post.new(hall_url, {'Content-Type' =>'application/json'})
-    req.body = data.to_s
-    Net::HTTP.new(uri.host, uri.port, :use_ssl => true).start {|http| http.request(req) }
+    req.body = data
+    Net::HTTP.start(uri.host, uri.port, :use_ssl => true) {|http| http.request(req) }
   end
 end
 
