83d9874bc68dd24327652ccc161c423a4196660c
Vincent Pochet <vincent.pochet@gmail.com>
2014-05-21 18:00:18 +0200

    ssl format for posted response

--- app/response.rb
+++ app/response.rb
@@ -6,7 +6,7 @@ class Response
     uri = URI(hall_url)
     req = Net::HTTP::Post.new(hall_url, {'Content-Type' =>'application/json'})
     req.body = data
-    Net::HTTP.new(uri.host, uri.port).start {|http| http.request(req) }
+    Net::HTTP.new(uri.host, uri.port, :use_ssl => true).start {|http| http.request(req) }
   end
 end
 
